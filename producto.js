195706c36ec4a23564511c3edc2adc29c91c91c3
Santiago Arias <santi99oca@gmail.com>
2024-05-20 22:41:19 -0300

    Solo muestra la parte entera del precio

--- producto.js
+++ producto.js
@@ -1,7 +1,7 @@
 const data = JSON.parse(localStorage.getItem('producto'));
 
 document.getElementById('nombre-producto').innerHTML = data.ProductTitle;
-document.getElementById('precio-producto').innerHTML = Math.random() * 80000 + 20000;
+document.getElementById('precio-producto').innerHTML = parseInt(Math.random() * 80000 + 20000);
 document.getElementById('imagen-producto').src = data.ImageURL;
 const descripcion = document.getElementById('descripcion-producto');
 for(key in data){
